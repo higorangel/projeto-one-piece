0df255b8847675c1297b71515eaa17217b00d5a2
higorangel <143042047+higorangel@users.noreply.github.com>
2023-10-08 10:37:18 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,43 @@
+/*
+ O que precisamos fazer? - quando passar o mouse em cima do personagem na lista temos que adicionar a borda azul de seleção na imagem pequena do personagem e mostrar a imagem, o nome e o texto grande do personagem que está selecionado
+
+    OBJETIVO 1 - quando passar o mouse em cima do personagem na listagem, devemos selecioná-lo
+        passo 1 - pegar os personagens no JS pra poder verificar quando o usuário passar o mouse em cima de um deles
+        passo 2 - adicionar a classe selecionado no personagem que o usuário passar o cursor do mouse
+        passo 3 - verificar se já exista um personagem selecionado, se sim, devemos remover a seleção dele 
+
+    OBJETIVO 2 - quando passar o mouse em cima do personagem na listagem, trocar a imagem, o nome e a descrição do personagem grande
+        passo 1 - pegar o elemento do personagem grande pra adicionar as informações nele
+        passo 2 - alterar a imagem do personagem grande
+        passo 3 - alterar o nome do personagem grande
+        passo 4 - alterar a descrição do personagem grande
+*/
+
+
+const botoes = document.querySelectorAll('.botao');
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+    botao.addEventListener("click", () => {
+        desselecionarBotao();
+        desselecionarPersonagem();
+
+        botao.classList.add("selecionado");
+        personagens[indice].classList.add("selecionado");
+
+    })
+})
+
+
+function desselecionarPersonagem() {
+    const personagemSelecionado = document.querySelector(".personagem.selecionado");
+    personagemSelecionado.classList.remove("selecionado");
+}
+
+function desselecionarBotao() {
+    const botaoSelecionado = document.querySelector(".botao.selecionado");
+    botaoSelecionado.classList.remove("selecionado");
+}
+// botoes.addEventListener("click", () => {
+// console.log("clicou no botoao");
+// });
